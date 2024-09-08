19fb64e45a6288a322fdd4aa9b32885e0d7011bb
rayhanshaik <shaikrayhan222@gmail.com>
2024-09-08 12:19:13 -0500

    Add files via upload
    This is my Java Class exercises.

---
+++ Comments.java
@@ -0,0 +1,20 @@
+//Program comments are nonexecuting statements you add to a file for documentation.
+
+/**
+Program comments are nonexecuting statements you add to a file for documentation.
+*/
+
+/*
+Program comments are nonexecuting statements you add to a file for documentation.
+ */
+import javax.swing.JOptionPane;
+
+public class Comments {
+
+	public static void main(String[] args) {
+		//System.out.println("Program comments are nonexecuting statements you add to a file for documentation.");
+		JOptionPane.showMessageDialog(null, "Program comments are nonexecuting statements you add to a file for documentation.");
+
+	}
+
+}
---
+++ MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+/**
+Author: Rayhan Shaik
+Date: 9/8/2024
+Description: When this program executes, it will print the quote by T-800 Terminator from the movie, The Terminator, made in the year 1984.
+*/
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		System.out.println("\"I'll be  back\"");
+		System.out.println("The Terminator");
+		System.out.println("T-800 Terminator played by Arnold Schwarzenegger");
+		System.out.println("1984");
+	}
+
+}
